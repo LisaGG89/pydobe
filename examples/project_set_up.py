46e5274b34c1bb39cd5fa38a9abdfe310611f83c
lisa <lisa@blinkink.co.uk>
2023-02-03 13:02:08 +0000

    :memo: Update project examples

--- examples/project_set_up.py
+++ examples/project_set_up.py
@@ -1,4 +1,7 @@
 import pydobe
 
-# Should return a project python object
+# Retrieve project object
 print(pydobe.objects.app.project)
+
+# With after effects open you should receive a project object
+# With after effects closed you should receive a Connection Error
