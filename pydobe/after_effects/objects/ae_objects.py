8318b87d3db7c8ac73a8a9192a2bbb74e185d1a5
lisa <lisa@blinkink.co.uk>
2023-02-07 16:38:41 +0000

    ✨ Add method to save project

--- pydobe/after_effects/objects/ae_objects.py
+++ pydobe/after_effects/objects/ae_objects.py
@@ -31,3 +31,44 @@ class Project(PydobeBaseObject):
             return self._eval_on_this_object('close(CloseOptions.SAVE_CHANGES)')
         else:
             return self._eval_on_this_object('close(CloseOptions.DO_NOT_SAVE_CHANGES)')
+
+    def save(self, path: str = None):
+        """This will save the current scene"""
+        if path:
+            file = File(path, **eval_script_returning_object(f'File("{path}")'))
+            extend_file_object = format_to_extend(file)
+            return self._eval_on_this_object(f'save({extend_file_object})')
+        else:
+            return self._eval_on_this_object('save()')
+
+
+# ADOBE GENERAL OBJECTS
+
+# ADOBE
+
+class File(PydobeBaseObject):
+    def __init__(self, path=None, pydobe_id=None):
+        super().__init__(pydobe_id)
+
+    def __str__(self):
+        return self.full_name
+
+    # PROPERTIES
+
+    "The full path name"
+
+    @property
+    def full_name(self):
+        return self._eval_on_this_object('fullName')
+
+    "The file name portion of the absolute URI, without the path specification."
+
+    @property
+    def name(self):
+        return self._eval_on_this_object('name')
+
+    "The path portion of the absolute URI, without the file name"
+
+    @property
+    def path(self):
+        return self._eval_on_this_object('path')
