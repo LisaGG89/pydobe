ccc17d75d514aa550972ddbe322951117592e2fb
lisa <lisa@blinkink.co.uk>
2023-02-07 16:55:13 +0000

    📝 Correct app naming

--- examples/working_with_scene_files.py
+++ examples/working_with_scene_files.py
@@ -1,30 +1,30 @@
 import pydobe
 
-ae = pydobe.objects.app
+app = pydobe.objects.app
 
 # Open a Project
 project_path = "path/to/my/project.aep"
-ae.open(project_path)
+app.open(project_path)
 
 # Get path of current project
-current_project = ae.project.file
+current_project = app.project.file
 print(current_project)
 
 # # Save a Project
-ae.project.save()
+app.project.save()
 
 # Save a Project to a new path
 new_path = "path/to/my/new/project.aep"
-ae.project.save(new_path)
+app.project.save(new_path)
 
 # # Close a Project
-ae.project.close()  # This will display a user prompt
+app.project.close()  # This will display a user prompt
 # ae.project.close(save=True)  # This will save before opening a new project
 # ae.project.close(save=False)  # This will not save before opening a new project
 
 
 # Create a new Project
-ae.new_project()  # This will display a user prompt
+app.new_project()  # This will display a user prompt
 # ae.new_project(save=True)  # This will save before opening a new project
 # ae.new_project(save=False)  # This will not save before opening a new project
 
