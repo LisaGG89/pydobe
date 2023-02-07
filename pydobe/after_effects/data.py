37dd525bddf097a1ddd85864345f140c383c1e94
lisa <lisa@blinkink.co.uk>
2023-02-07 18:10:08 +0000

    ⚗️ Add unique dictionary for labels

---
+++ pydobe/after_effects/data.py
@@ -0,0 +1,19 @@
+label_dictionary = IntStringDict({
+    0: 'None',
+    1: 'Red',
+    2: 'Yellow',
+    3: 'Aqua',
+    4: 'Pink',
+    5: 'Lavender',
+    6: 'Peach',
+    7: 'Sea Foam',
+    8: 'Blue',
+    9: 'Green',
+    10: 'Purple',
+    11: 'Orange',
+    12: 'Brown',
+    13: 'Fuchsia',
+    14: 'Cyan',
+    15: 'Sandstone',
+    16: 'Dark Green'
+})
