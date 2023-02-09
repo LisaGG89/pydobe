05b27612991f6c7384a1dd6bb4da0553e97aa99a
lisa <lisa@blinkink.co.uk>
2023-02-09 16:52:30 +0000

    ✨ Add Viewer attributes

--- pydobe/after_effects/objects/ae_objects.py
+++ pydobe/after_effects/objects/ae_objects.py
@@ -533,3 +533,29 @@ class ImportOptions(PydobeBaseObject):
 class Viewer(PydobeBaseObject):
     def __init__(self, pydobe_id=None):
         super().__init__(pydobe_id)
+
+    # PROPERTIES
+
+    """When true, indicates if the viewer panel is focused, and thereby frontmost."""
+
+    @property
+    def active(self) -> bool:
+        return self._eval_on_this_object('active')
+
+    """When true, indicates if the viewer panel is at its maximized size."""
+
+    @property
+    def maximised(self) -> bool:
+        return self._eval_on_this_object('maximized')
+
+    @maximised.setter
+    def maximised(self, value: bool):
+        extend_value = format_to_extend(value)
+        self._eval_on_this_object(f'maximized = {extend_value}')
+
+    # FUNCTIONS
+
+    def set_active(self) -> bool:
+        """Moves the viewer panel to the front and places focus on it, making it active."""
+        return self._eval_on_this_object('setActive()')
+
