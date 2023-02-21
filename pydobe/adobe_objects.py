c7fb80316c9c22bee8fb38ca2bf8601493504239
lisa <lisa@blinkink.co.uk>
2023-02-21 16:04:42 +0000

    ♻️ Add object type to base object

--- pydobe/adobe_objects.py
+++ pydobe/adobe_objects.py
@@ -2,8 +2,8 @@ from pydobe.core import PydobeBaseObject
 
 
 class File(PydobeBaseObject):
-    def __init__(self, pydobe_id=None):
-        super().__init__(pydobe_id)
+    def __init__(self, pydobe_id=None, object_type=None):
+        super().__init__(pydobe_id, object_type)
 
     def __str__(self):
         return self.full_name
@@ -14,16 +14,16 @@ class File(PydobeBaseObject):
 
     @property
     def full_name(self) -> str:
-        return self._eval_on_this_object("fullName")
+        return self._eval_on_object("fullName")
 
     "The file name portion of the absolute URI, without the path specification."
 
     @property
     def name(self) -> str:
-        return self._eval_on_this_object("name")
+        return self._eval_on_object("name")
 
     "The path portion of the absolute URI, without the file name"
 
     @property
     def path(self) -> str:
-        return self._eval_on_this_object("path")
+        return self._eval_on_object("path")
