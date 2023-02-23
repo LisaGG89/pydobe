8d7d7e998122a045312623d3996855fa213b9514
lisa <lisa@blinkink.co.uk>
2023-02-23 15:25:31 +0000

    ✨ Add adobe Folder object

--- pydobe/adobe_objects.py
+++ pydobe/adobe_objects.py
@@ -27,3 +27,20 @@ class File(PydobeBaseObject):
     @property
     def path(self) -> str:
         return self._eval_on_object("path")
+
+
+class Folder(PydobeBaseObject):
+    def __init__(self, pydobe_id=None, object_type=None):
+        super().__init__(pydobe_id, object_type)
+
+    """The name of the folder"""
+
+    @property
+    def name(self) -> str:
+        return self._eval_on_object("name")
+
+    "The path portion of the absolute URI, without the folder name"
+
+    @property
+    def path(self) -> str:
+        return self._eval_on_object("path")
