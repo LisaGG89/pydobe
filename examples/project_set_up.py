bd9b3bb8431c6d516e860e8c9a7e71a94eda78db
lisa <lisa@blinkink.co.uk>
2023-02-23 15:26:44 +0000

    ⚰️ Remove obsolete file

--- examples/project_set_up.py
+++
@@ -1,7 +0,0 @@
-import pydobe
-
-# Retrieve project object
-print(pydobe.objects.app.project)
-
-# With after effects open you should receive a project object
-# With after effects closed you should receive a Connection Error
